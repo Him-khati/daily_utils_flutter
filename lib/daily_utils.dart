68aacd0d995ba260dd0027fcd0ca1a0b5a4564c2
Himanshu <himanshu.temp@gig4ce.com>
2024-05-28 11:45:44 +0530

    Import statement changed to export

--- lib/daily_utils.dart
+++ lib/daily_utils.dart
@@ -1,8 +1,8 @@
 library daily_utils;
 
-import 'colors/colors_util.dart';
+export 'colors/colors_util.dart';
 
 //Widgets
-import 'widgets/gap/horizontal_gap_widget.dart';
-import 'widgets/gap/vertical_gap_widget.dart';
+export 'widgets/gap/horizontal_gap_widget.dart';
+export 'widgets/gap/vertical_gap_widget.dart';
 
